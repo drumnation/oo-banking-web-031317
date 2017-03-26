1088330fd74aadd72a87bf6aab3a610e76c5bcae
David Mieloch <davidmieloch@gmail.com>
2017-03-26 01:43:00 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+  # don't need to change names - do need to change balance and status
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+    # initializes bank account with name and
+    # defaults of $1000 for balance and 'open' for status
+  end
+
+  def deposit(money)
+    @balance += money
+    # adds money to the balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+    # returns string message with balance amount
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+    # a bank account is valid if it is open and balance greater than 0
+  end
+
+  def close_account
+    @status = "closed"
+    # closes an account
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,46 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  # status is the only value that needs to change
+
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+    # 3 arguments passed in with status initialized with a default
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? && (sender.balance >= amount)
+    # sender and receiver accounts 'open' and balance greater than 0
+    # and the sender's balance is greater than or equal to amount being sent
+  end
+
+  def execute_transaction
+    if @status == "pending" && valid?
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+    # if status value is 'pending' and transaction is valid
+    # subtract amount from sender and add to receiver, update status to complete
+    # otherwise reject the transaction and return a message
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      receiver.balance -= @amount
+      sender.balance += @amount
+      @status = "reversed"
+    end
+    # if transaction status is complete
+    # receiver subtracts amount from their balance
+    # sender adds amount to their balance
+    # status is updated to 'reversed'
+  end
+
 end
